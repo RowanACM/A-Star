070e4d6abe9ee380521590060a49c29c18d50e90
Nick LaPosta <lapost48@students.rowan.edu>
2016-09-23 11:41:40 -0400

    Implemented getNeighbors

--- src/astar/AStarMain.java
+++ src/astar/AStarMain.java
@@ -2,6 +2,9 @@ package astar;
 
 import gui.AStarWindow;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 /**
  * Created by lapost48 on 9/23/2016.
  */
@@ -9,10 +12,17 @@ public class AStarMain {
 
     public static void main(String[] args) {
 
-
         AStarWindow w = new AStarWindow("A*", 15, 15);
 
+        PriorityQueue<Path>  frontier = new PriorityQueue<>();
+        ArrayList<Path> closed = new ArrayList<>();
 
+        boolean finished = false;
+        while(!finished) {
+
+
+
+        }
 
     }
 
